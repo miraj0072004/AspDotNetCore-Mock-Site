2b15566eb1db55d522ec76b9a9ce0c2faad5548e
Mohajireen Miraj <mohajireen.miraj@ifsworld.com>
2019-12-09 10:13:09 +0530

    User Delete Confirmation

--- EmployeeManagement/wwwroot/js/CustomScript.js
+++ EmployeeManagement/wwwroot/js/CustomScript.js
@@ -1,1 +1,12 @@
-﻿
+﻿function confirmDelete(uniqueId, isDeleteClicked) {
+
+    var confirmDeleteSpan = "confirmDeleteSpan_" + uniqueId;
+    var deleteSpan = "deleteSpan_" + uniqueId;
+    if (isDeleteClicked) {
+        $("#" + confirmDeleteSpan).show();
+        $("#" + deleteSpan).hide();
+    } else {
+        $("#" + confirmDeleteSpan).hide();
+        $("#" + deleteSpan).show();
+    }
+}
